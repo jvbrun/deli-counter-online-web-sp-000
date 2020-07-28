382936cdd6e787ff0b9106347edd59a0978f19c2
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-07-28 00:55:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,11 +11,7 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty." 
   else
-    place = 1 
-    katz_deli.each do |index| 
-      puts "The line is currently: #{place}. #{index}"
-      place += 1 
-    end
+      katz_deli.each_with_index {|val, index| puts "#{index}. #{val}" }
   end
 end
 
